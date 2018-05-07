4908ed505988509d2edfafda4ff9542d95afaf57
Maxime Bonhomme <hello.bonhomme@gmail.com>
2018-05-07 22:03:10 +0100

    add shopify test store - test GET shop info and products with graphql

---
+++ app.config.js
@@ -0,0 +1,7 @@
+const config = {
+  shopifyURL: 'https://bonhomme-test-store.myshopify.com/api/graphql',
+  shopifyToken: '5be18200bdbe82f6d250bb9ba20edcc5',
+  shopifyHeaders: 'X-Shopify-Storefront-Access-Token'
+};
+
+export default config;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import withData from '../lib/withData';
 
 import App from '../components/App';
+import Shop from '../components/molecules/Shop';
 
 function Home(data) {
   return (
     <App>
+      <Shop />
       <div>
         home
       </div>
     </App>
   );
 }
+
 
 export default withData(Home);
