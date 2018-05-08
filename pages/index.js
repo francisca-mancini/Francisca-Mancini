511bcafd3c366bac5dd88ae92d9a7f7762e48410
Maxime Bonhomme <hello.bonhomme@gmail.com>
2018-05-08 15:15:04 +0100

    add static assets support

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,12 @@ import withData from '../lib/withData';
 import App from '../components/App';
 import Shop from '../components/molecules/Shop';
 
+import logo from '../static/images/logo.jpg';
+
 function Home(data) {
   return (
     <App>
+      <img src={logo} alt="logo" />
       <Shop />
       <div>
         home
