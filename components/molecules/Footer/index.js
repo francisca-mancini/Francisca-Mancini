217e8d74b40045b654b225e4ceae947de219cc09
Maxime Bonhomme <hello.bonhomme@gmail.com>
2018-06-06 15:35:00 +0100

    footer wip

--- components/molecules/Footer/index.js
+++ components/molecules/Footer/index.js
@@ -1,9 +1,82 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MediaQuery from 'react-responsive';
+import classNames from 'classnames/bind';
 
-export default function Footer() {
+import PageWrap from '../../atoms/PageWrap';
+import { InlineGrid } from '../../atoms/Grid';
+import Link from '../../atoms/Link';
+import Spacing from '../../atoms/Spacing';
+import Heading from '../../atoms/Heading';
+
+import logoWhite from '../../../static/images/sprites/logo-white.svg';
+import logoBlack from '../../../static/images/sprites/logo-black.svg';
+import logoMini from '../../../static/images/sprites/logo-mini-center.svg';
+
+import generalStyles from './general.css';
+
+const cx = classNames.bind({ ...generalStyles });
+
+export default function Footer({ isLight }) {
+  const footerClassName = cx('footer', {
+    isLight: isLight,
+    isDark: !isLight
+  });
+  const logoSrc = isLight ? logoWhite : logoBlack;
+  const linkColor = isLight ? 'white' : 'black';
+
+  const NavLink = ({ children, href }) => {
+    return (
+      <Spacing size={10} position="x">
+        <Link href={href}>
+          <Heading
+            tag="span"
+            uppercase
+            size="xxxs"
+            weight="semilight"
+            tracking="wide"
+            color={linkColor}
+          >
+            {children}
+          </Heading>
+        </Link>
+      </Spacing>
+    );
+  };
+
   return (
-    <div className="bg-black p-2 text-red">
-      footer
-    </div>
-  )
+    <footer className={footerClassName}>
+      <PageWrap>
+        <InlineGrid direction={['col', 'col', 'row']}>
+          <Link href="/" className="leading-none flex items-center">
+            <MediaQuery minDeviceWidth={768} values={{ deviceWidth: 800 }}>
+              {matches => {
+                if (matches) {
+                  return <img src={logoSrc} width={243} height={16} />;
+                } else {
+                  return <img src={logoMini} width={156} height={45} />;
+                }
+              }}
+            </MediaQuery>
+          </Link>
+          <InlineGrid>
+            <NavLink href="/about">About</NavLink>
+            <NavLink href="/contact">Contact</NavLink>
+            <NavLink href="https://instagram.com">Instagram</NavLink>
+            <NavLink href="/help">Help</NavLink>
+            <NavLink href="/newsletter">Newsletter</NavLink>
+            <NavLink href="/legals">Legals</NavLink>
+          </InlineGrid>
+        </InlineGrid>
+      </PageWrap>
+    </footer>
+  );
 }
+
+Footer.propTypes = {
+  isLight: PropTypes.bool
+};
+
+Footer.defaultProps = {
+  isLight: false
+};
