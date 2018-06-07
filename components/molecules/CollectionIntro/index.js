d67cf247adc08397ec8782e2e2f299ff24bdd3f7
Maxime Bonhomme <hello.bonhomme@gmail.com>
2018-06-07 17:12:45 +0100

    update CollectionIntro with WordHover

--- components/molecules/CollectionIntro/index.js
+++ components/molecules/CollectionIntro/index.js
@@ -6,20 +6,25 @@ import Button from '../../atoms/Button';
 import Spacing from '../../atoms/Spacing';
 import MaxWidth from '../../atoms/MaxWidth';
 
+import WordHover from '../../molecules/WordHover';
+
+import bubbleImage from '../../../static/images/_temp/bubble.jpg';
+
 export default function CollectionIntro() {
   return (
     <Fragment>
-      <Heading size="xxl" center>
+      <Heading weight="semilight" size="xxl" center>
         Collection N˚1: Maps, Travel
       </Heading>
       <Spacing size={50}>
         <MaxWidth value={1000} center>
-          <Paragraph size="xxl" tag="div" center>
+          <Paragraph weight="semilight" size="xxl" tag="div" center>
             This first collection maps out a fantastic world. Each fragrance,
             inspired by a place and a time, embodies the spirit of that magical
-            situation. This historical map is chronologically incorrect and
-            these diverse moments in history coexist here and now. Every time
-            you wear these perfumes, ever time you layer them, you get to
+            situation. This historical map is{' '}
+            <WordHover image={bubbleImage}>chronologically</WordHover> incorrect
+            and these diverse moments in history coexist here and now. Every
+            time you wear these perfumes, ever time you layer them, you get to
             reconstitute the map, write your own story.
           </Paragraph>
         </MaxWidth>
