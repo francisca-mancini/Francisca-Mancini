c217f16d0881fd1af270735e8dac198478ae4dbd
Maxime Bonhomme <hello.bonhomme@gmail.com>
2018-06-14 23:26:38 +0100

    story wip

--- components/organisms/product/Story/index.js
+++ components/organisms/product/Story/index.js
@@ -2,6 +2,8 @@ import Spacing from '../../../atoms/Spacing';
 import { Grid, GridItem } from '../../../atoms/Grid';
 import Paragraph from '../../../atoms/Paragraph';
 import Heading from '../../../atoms/Heading';
+import PageWrap from '../../../atoms/PageWrap';
+import MaxWidth from '../../../atoms/MaxWidth';
 
 import generalStyles from './general.css';
 
@@ -9,34 +11,88 @@ export default function Story() {
   return (
     <Spacing size={80} type="padding">
       <div className={generalStyles.story}>
-        <Spacing size={80} type="padding">
-          <Grid gap={30} align="center">
-            <GridItem columnSize={6}>
-              <Spacing size={30} position="b">
-                <Heading size="s" weight="semilight" center>
-                  Fragrance notes
-                </Heading>
-              </Spacing>
-            </GridItem>
-            <GridItem columnSize={6}>
-              <Spacing size={30} position="b">
-                <Heading size="s" weight="semilight" center>
-                  Story
-                </Heading>
-              </Spacing>
-              <Paragraph size="xl">
-                {'     '}Bark is navigating down the river, between the temples
-                of Luxor and Karnak, it stops in front of a small temple. As you
-                walk in, there is a room to the left. A ray of light comes from
-                the sky through a small window high up, cutting through incense
-                smoke. There is an altar carved in stone, and to the right, on
-                the floor there is an ancient vase. Inside this vase, set aside
-                for ceremonial offerings, lays Atlantica, a magical ritual
-                perfume made out of the most precious oils.
-              </Paragraph>
-            </GridItem>
-          </Grid>
-        </Spacing>
+        <PageWrap>
+          <Spacing size={80} type="padding">
+            <Grid gap={30} align="center">
+              <GridItem columnSize={6}>
+                <Spacing size={30} position="b">
+                  <Heading size="s" weight="semilight" center>
+                    Fragrance notes
+                  </Heading>
+                </Spacing>
+
+                <Spacing size={10}>
+                  <MaxWidth value={450} center>
+                    <Grid gap={10} align="center">
+                      <GridItem columnSize={3}>
+                        <Heading uppercase size="xxl">
+                          TOP
+                        </Heading>
+                      </GridItem>
+                      <GridItem columnSize={9}>
+                        <Heading size="xxl" font="jenson">
+                          Ambrette / Coconut water
+                        </Heading>
+                      </GridItem>
+                    </Grid>
+                  </MaxWidth>
+                </Spacing>
+
+                <Spacing size={10}>
+                  <MaxWidth value={450} center>
+                    <Grid gap={10} align="center">
+                      <GridItem columnSize={3}>
+                        <Heading uppercase size="xxl">
+                          Heart
+                        </Heading>
+                      </GridItem>
+                      <GridItem columnSize={9}>
+                        <Heading size="xxl" font="jenson">
+                          Patchouli Leaves
+                        </Heading>
+                      </GridItem>
+                    </Grid>
+                  </MaxWidth>
+                </Spacing>
+
+                <Spacing size={10}>
+                  <MaxWidth value={450} center>
+                    <Grid gap={10} align="center">
+                      <GridItem columnSize={3}>
+                        <Heading uppercase size="xxl">
+                          Base
+                        </Heading>
+                      </GridItem>
+                      <GridItem columnSize={9}>
+                        <Heading size="xxl" font="jenson">
+                          Coco Absolu / Wold musk
+                        </Heading>
+                      </GridItem>
+                    </Grid>
+                  </MaxWidth>
+                </Spacing>
+              </GridItem>
+              <GridItem columnSize={6}>
+                <Spacing size={30} position="b">
+                  <Heading size="s" weight="semilight" center>
+                    Story
+                  </Heading>
+                </Spacing>
+                <Paragraph font="jenson" size="xl">
+                  {'     '}Bark is navigating down the river, between the
+                  temples of Luxor and Karnak, it stops in front of a small
+                  temple. As you walk in, there is a room to the left. A ray of
+                  light comes from the sky through a small window high up,
+                  cutting through incense smoke. There is an altar carved in
+                  stone, and to the right, on the floor there is an ancient
+                  vase. Inside this vase, set aside for ceremonial offerings,
+                  lays Atlantica, a magical ritual perfume made out of the most
+                  precious oils.
+                </Paragraph>
+              </GridItem>
+            </Grid>
+          </Spacing>
+        </PageWrap>
       </div>
     </Spacing>
   );
--- pages/product.js
+++ pages/product.js
@@ -11,7 +11,9 @@ export default function Product() {
     <App hasTopPad={false}>
       <PageWrap>
         <Hero />
-        <Story />
+      </PageWrap>
+      <Story />
+      <PageWrap>
         <YouMightLike />
       </PageWrap>
     </App>
