7db98600a3e342d0c33bb38fc41e1256ee7afced
Maxime Bonhomme <hello.bonhomme@gmail.com>
2018-07-17 16:37:46 +0100

    real data for you might like section

--- components/organisms/product/YouMightLike/index.js
+++ components/organisms/product/YouMightLike/index.js
@@ -1,29 +1,34 @@
+import React, { Fragment } from 'react';
+
 import Spacing from '../../../atoms/Spacing';
 import { Grid, GridItem } from '../../../atoms/Grid';
 import ProductThumbnail from '../../../molecules/ProductThumbnail';
 import Heading from '../../../atoms/Heading';
 
-export default function YouMightLike() {
+export default function YouMightLike({ products }) {
   return (
     <Spacing size={80} type="padding">
-      <Spacing size={60} position="b" type="padding">
-        <Heading size="s" center>
-          You might like
-        </Heading>
-      </Spacing>
-      <Spacing size={80} position="b" type="padding">
-        <Grid gap={30}>
-          <GridItem columnSize={4}>
-            <ProductThumbnail />
-          </GridItem>
-          <GridItem columnSize={4}>
-            <ProductThumbnail />
-          </GridItem>
-          <GridItem columnSize={4}>
-            <ProductThumbnail />
-          </GridItem>
-        </Grid>
-      </Spacing>
+      {products &&
+        products.length && (
+          <Fragment>
+            <Spacing size={60} position="b" type="padding">
+              <Heading size="s" center>
+                You might like
+              </Heading>
+            </Spacing>
+            <Spacing size={80} position="b" type="padding">
+              <Grid gap={30}>
+                {products.map((item, index) => {
+                  return (
+                    <GridItem key={index} columnSize={4}>
+                      <ProductThumbnail product={item.node} />
+                    </GridItem>
+                  );
+                })}
+              </Grid>
+            </Spacing>
+          </Fragment>
+        )}
     </Spacing>
   );
 }
